80a56e7cfe1e3a672762e4ff11f3754c212a4cec
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-08-13 19:53:18 -0300

    novo componente e mudanças
    componente populares e algumas mudanças no css.

--- MovieSearch/src/App.jsx
+++ MovieSearch/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import MovieSearchBar from "./Componentes/MovieSearchBar";
 import Titulo from "./Componentes/Titulo";
+import Populares from "./Componentes/Populares";
 import Rodape from "./Componentes/Rodape";
 function App() {
   return (
     <>
       <Titulo />
       <MovieSearchBar/>
+      <h1 className="Populares-title">Populares</h1>
+      <Populares/>
       <Rodape/>
     </>
   );
---
+++ MovieSearch/src/Componentes/Populares.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./Populares.css";
+
+const Populares = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      try {
+        const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`
+        );
+        const data = await res.json();
+        setMovies(data.results.slice(0, 10)); //busacando 10 filmes
+      } catch (err) {
+        console.error("Erro ao buscar filmes populares:", err);
+      }
+    };
+
+    fetchPopularMovies();
+  }, []);
+
+  return (
+    <div className="populares-container">
+      {movies.map((movie) => (
+        <div key={movie.id} className="populares-item">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className="populares-poster"
+          />
+          <p className="populares-title">{movie.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Populares;

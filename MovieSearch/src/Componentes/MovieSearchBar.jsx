622ab730ce7358d8b0f421d2a4ec69d0d682b19b
Apenas1Hugo <hugorodriguesm27@gmail.com>
2025-08-18 19:43:14 -0300

    inicio do componente de busca
    comecei a mexer no componente de buscas e fiz algumas alterações no css.

--- MovieSearch/src/Componentes/MovieSearchBar.jsx
+++ MovieSearch/src/Componentes/MovieSearchBar.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from "react";
+import "./MovieSearchBar.css";
 
 const MovieSearchBar = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <div className="movie-search-bar">
+      <h2 className="movie-search-bar__title">
+        que filme você está procurando?
+      </h2>
+      <div className="movie-search-bar__controls">
+        <input
+          type="text"
+          className="movie-search-bar__input"
+          placeholder="digite o nome do filme"
+        />
+        <button className="movie-search-bar__button">Buscar</button>
+      </div>
+    </div>
+  );
+};
 
-export default MovieSearchBar
-
+export default MovieSearchBar;
